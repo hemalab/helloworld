da933a1f4a5f68faaf3f2e5bebf8a80b2c5668ef
hemalab <hemalatha.a.b@capgemini.com>
2017-02-01 13:19:30 +0530

    Add files via upload

---
+++ MyFirst.java
@@ -0,0 +1,23 @@
+package tdi.javaFLP.languageBasic;
+
+/**
+ * 
+ * @author vivsinha
+ * This is my first java class, 
+ * And in this class I am printing (displaying) a message on console
+ */
+
+public class MyFirst {
+
+	/**
+	 * Main method - this is staring point of my program.
+	 * When I run the program, 
+	 * JVM will look for this method and program will start execution  
+	 */
+	
+public static void main(String[] args) {
+		
+		// using System.out.println to print a message on console
+		System.out.println("Yeeeeeee, My first Java program. :-) ");
+	}
+}
